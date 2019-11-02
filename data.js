b372bdc73635ad7abd2659516fb63f34d1351b2e
Pete Nelson <nelsonp@aetna.com>
2019-11-02 16:03:06 -0600

    initial

---
+++ data.js
@@ -0,0 +1,116 @@
+const SKILL_TYPES = {
+    LANGUAGE: 'LANGUAGE',
+    UI_FRAMEWORK: 'UI_FRAMEWORK',
+    UI: 'UI',
+    SERVER: 'SERVER',
+    DESIGN: 'DESIGN',
+    DATABASE: 'DATABASE',
+    TESTING: 'TESTING',
+}
+exports.skillTypes = SKILL_TYPES;
+exports.skills = [{
+    type: SKILL_TYPES.LANGUAGE,
+    name: 'JavaScript'
+}, {
+    type: SKILL_TYPES.LANGUAGE,
+    name: 'Java',
+}, {
+    type: SKILL_TYPES.LANGUAGE,
+    name: 'Scala',
+}, {
+    type: SKILL_TYPES.LANGUAGE,
+    name: 'Python',
+}, {
+    type: SKILL_TYPES.LANGUAGE,
+    name: 'Swift',
+}, {
+    type: SKILL_TYPES.UI_FRAMEWORK,
+    name: 'React/Redux',
+}, {
+    type: SKILL_TYPES.UI_FRAMEWORK,
+    name: 'Angular',
+}, {
+    type: SKILL_TYPES.UI_FRAMEWORK,
+    name: 'Vue',
+}, {
+    type: SKILL_TYPES.UI_FRAMEWORK,
+    name: 'ExtJS',
+}, {
+    type: SKILL_TYPES.UI,
+    name: 'CSS',
+}, {
+    type: SKILL_TYPES.UI,
+    name: 'SASS',
+}, {
+    type: SKILL_TYPES.UI,
+    name: 'PostCSS',
+}, {
+    type: SKILL_TYPES.UI,
+    name: 'SVG',
+}, {
+    type: SKILL_TYPES.UI,
+    name: 'HTML5',
+}, {
+    type: SKILL_TYPES.SERVER,
+    name: 'Node',
+}, {
+    type: SKILL_TYPES.SERVER,
+    name: 'Express',
+}, {
+    type: SKILL_TYPES.SERVER,
+    name: 'Spring',
+}, {
+    type: SKILL_TYPES.SERVER,
+    name: 'Rest',
+}, , {
+    type: SKILL_TYPES.SERVER,
+    name: 'Soap',
+}, {
+    type: SKILL_TYPES.SERVER,
+    name: 'Kafka',
+}, {
+    type: SKILL_TYPES.SERVER,
+    name: 'Spark',
+}, {
+    type: SKILL_TYPES.DESIGN,
+    name: 'Illustrator',
+}, {
+    type: SKILL_TYPES.DESIGN,
+    name: 'Photoshop',
+}, {
+    type: SKILL_TYPES.DESIGN,
+    name: 'Sketch',
+}, {
+    type: SKILL_TYPES.DATABASE,
+    name: 'Postgres',
+}, {
+    type: SKILL_TYPES.DATABASE,
+    name: 'MySQL',
+}, {
+    type: SKILL_TYPES.DATABASE,
+    name: 'SQL Server',
+}, {
+    type: SKILL_TYPES.DATABASE,
+    name: 'Hadoop',
+}, {
+    type: SKILL_TYPES.DATABASE,
+    name: 'OpenTSDB',
+}, {
+    type: SKILL_TYPES.TESTING,
+    name: 'Enzyme',
+}, {
+    type: SKILL_TYPES.TESTING,
+    name: 'Karma',
+}, {
+    type: SKILL_TYPES.TESTING,
+    name: 'Chai',
+}, {
+    type: SKILL_TYPES.TESTING,
+    name: 'Sinon',
+}, {
+    type: SKILL_TYPES.TESTING,
+    name: 'Nightwatch',
+}, {
+    type: SKILL_TYPES.TESTING,
+    name: 'Cucumber',
+}]
